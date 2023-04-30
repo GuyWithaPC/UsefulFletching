02ae9cc3a8d36232cea72938be259e5575120c14
Max Petersen <maxvonpetersen@gmail.com>
2023-04-29 18:44:14 -0600

    Initial Commit

---
+++ src/main/java/io/guywithapc/usefulfletching/UsefulFletchingClient.java
@@ -0,0 +1,14 @@
+package io.guywithapc.usefulfletching;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UsefulFletchingClient implements ClientModInitializer {
+
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
